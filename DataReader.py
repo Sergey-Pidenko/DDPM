8f68fc546ddee7e2b3890031b6999cc4ceee8069
Sergei <125606014+Sergey-Pidenko@users.noreply.github.com>
2025-03-19 17:32:38 +0300

    Add files via upload

--- DataReader.py
+++ DataReader.py
@@ -4,9 +4,13 @@ from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
 class SuperResolutionDataset(Dataset):
-    def __init__(self, dir, size=10, transform=None):
+    def __init__(self, dir, size=None, transform=None):
         self.dir = dir
-        self.high_res_files = sorted(os.listdir(dir))[:size]
+        self.high_res_files = sorted(os.listdir(dir))
+        if size:
+            self.high_res_files = self.high_res_files[:size]
+        
+        # Применяйте преобразование по умолчанию, если не передано
         self.transform = transform if transform is not None else transforms.ToTensor()
 
     def __len__(self):
@@ -15,12 +19,12 @@ class SuperResolutionDataset(Dataset):
     def __getitem__(self, idx):
         high_res_path = os.path.join(self.dir, self.high_res_files[idx])
 
-        # Чтение изображений
-        high_res_image = Image.open(high_res_path).convert('RGB')
-        low_res_image = high_res_image.resize((128, 128), Image.BICUBIC)
+        with Image.open(high_res_path) as img:
+            high_res_image = img.convert('RGB')
+            low_res_image = high_res_image.resize((128, 128), Image.BICUBIC)
 
-        # Применение преобразований
-        high_res_image = self.transform(high_res_image)
-        low_res_image = self.transform(low_res_image)
+            # Применение преобразований для преобразования в тензоры
+            high_res_image = self.transform(high_res_image)
+            low_res_image = self.transform(low_res_image)
 
         return low_res_image, high_res_image
