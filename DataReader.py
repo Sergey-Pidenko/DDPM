16898da0430229072be0eb35a4cfb5aaaaafb86f
Sergei <125606014+Sergey-Pidenko@users.noreply.github.com>
2025-03-25 17:23:41 +0300

    Add files via upload

--- DataReader.py
+++ DataReader.py
@@ -1,17 +1,31 @@
 import os
+import torch
 from PIL import Image
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
+class NormalizeToMinusOneToOne(object):
+    def __call__(self, x):
+        return x * 2.0 - 1.0
+
+# Функция для преобразования в float32
+class ToFloat32(object):
+    def __call__(self, x):
+        return x.to(torch.float32)
+
 class SuperResolutionDataset(Dataset):
-    def __init__(self, dir, size=None, transform=None):
+    def __init__(self, dir, size=10000, transform=None):
         self.dir = dir
         self.high_res_files = sorted(os.listdir(dir))
         if size:
             self.high_res_files = self.high_res_files[:size]
         
-        # Применяйте преобразование по умолчанию, если не передано
-        self.transform = transform if transform is not None else transforms.ToTensor()
+        # Применяем последовательность преобразований
+        self.transform = transform if transform is not None else transforms.Compose([
+            transforms.ToTensor(),
+            NormalizeToMinusOneToOne(),
+            ToFloat32()
+        ])
 
     def __len__(self):
         return len(self.high_res_files)
@@ -23,7 +37,7 @@ class SuperResolutionDataset(Dataset):
             high_res_image = img.convert('RGB')
             low_res_image = high_res_image.resize((128, 128), Image.BICUBIC)
 
-            # Применение преобразований для преобразования в тензоры
+            # Применение преобразований
             high_res_image = self.transform(high_res_image)
             low_res_image = self.transform(low_res_image)
 
